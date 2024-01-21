9a96f2ca8b1601015181272a246e243a0cafa080
frepingonsalvese <94340132+frepingonsalvese@users.noreply.github.com>
2024-01-21 23:07:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,6 +19,6 @@ app.use("/*", (req, res) => {
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
-mongoose.connect('mongodb+srv://kirattechnologies:iRbi4XRDdM7JMMkl@cluster0.e95bnsi.mongodb.net/admin?authSource=admin&replicaSet=atlas-ue73sj-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+mongoose.connect('mongodb+srv://frepingonsalvese13:jszRR3zEp9F7ZRXT@cluster0.pnjdotk.mongodb.net', { dbName: "NewCourse" });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
